8f4e024314670aa355d95ba56907a1ad825a3758
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-04-02 05:44:30 +0600

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "royal-blue": "#2405F2",
+        "tint-blue": "#1C1E53",
+        "dark-blue": "#282938",
+        yellow: "#FCD980",
+        gray: "#F4F6FC",
+        accent: "#EEF4FA",
       },
     },
   },
