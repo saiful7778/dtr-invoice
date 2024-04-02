691862b89cb2c6d4f5b3cdc6a07d5005571d58b9
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-04-02 10:00:32 +0600

    add register form schema

--- src/lib/schemas/authentication.js
+++ src/lib/schemas/authentication.js
@@ -14,3 +14,23 @@ export const loginSchema = yup.object().shape({
     })
     .required("Password is required"),
 });
+
+export const registerSchema = yup.object().shape({
+  fullName: yup.string().required("Name is required"),
+  email: yup
+    .string()
+    .email("Enter a valid email")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .min(5, "Password minimum 6 characters")
+    .matches(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{6,}$/, {
+      message:
+        "Password must required: Capital letter and a Special character.",
+    })
+    .required("Password is required"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Password must match")
+    .required("Cofirm password is required"),
+});
