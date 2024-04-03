d2bafd109d4d3f89066fb0790269fdd83d6a626e
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-04-03 10:39:53 +0600

    add user server action schema

--- src/lib/schemas/authentication.js
+++ src/lib/schemas/authentication.js
@@ -34,3 +34,19 @@ export const registerSchema = yup.object().shape({
     .oneOf([yup.ref("password"), null], "Password must match")
     .required("Cofirm password is required"),
 });
+
+export const createUserSchema = yup.object().shape({
+  fullName: yup.string().required("Name is required"),
+  email: yup
+    .string()
+    .email("Enter a valid email")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .min(5, "Password minimum 6 characters")
+    .matches(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{6,}$/, {
+      message:
+        "Password must required: Capital letter and a Special character.",
+    })
+    .required("Password is required"),
+});
