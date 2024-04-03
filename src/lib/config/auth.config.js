d3794c03537ea1ebba0d24a72433cc7a77cb6a9b
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-04-03 10:38:49 +0600

    add edge com config options

---
+++ src/lib/config/auth.config.js
@@ -0,0 +1,27 @@
+import db from "@/lib/db";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+const authConfig = {
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: "jwt",
+    maxAge: 4 * 60 * 60,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: process.env.NODE_ENV === "development",
+  pages: {
+    signIn: "/authentication/login",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) session.user.role = token.role;
+      return session;
+    },
+  },
+};
+
+export default authConfig;
