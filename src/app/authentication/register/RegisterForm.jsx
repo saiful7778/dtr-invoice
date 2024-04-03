3593d3f6e229f4b8b955a0fa53cf62f59512abbc
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-04-03 10:40:24 +0600

    add register user function

--- src/app/authentication/register/RegisterForm.jsx
+++ src/app/authentication/register/RegisterForm.jsx
@@ -1,19 +1,50 @@
 "use client";
 import { Input } from "@/components/formik/Input";
 import Password from "@/components/formik/Password";
+import createUser from "@/lib/actions/user/createUser";
 import { registerSchema } from "@/lib/schemas/authentication";
 import { Form, Formik } from "formik";
-import { Button } from "keep-react";
+import { Button, Spinner } from "keep-react";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import Alert from "@/lib/config/alert.config";
 
 const RegisterForm = () => {
+  const [spinner, setSpinner] = useState(false);
+
   const initialValues = {
     fullName: "",
     email: "",
     password: "",
     confirmPassword: "",
   };
-  const handleSubmit = async (e) => {
-    console.log(e);
+
+  const handleReset = (resetForm) => {
+    return () => {
+      resetForm();
+      setSpinner(false);
+    };
+  };
+
+  const handleSubmit = async (e, { resetForm }) => {
+    setSpinner(true);
+    const reset = handleReset(resetForm);
+    try {
+      const userData = {
+        fullName: e.fullName,
+        email: e.email,
+        password: e.password,
+      };
+      await createUserData(userData);
+    } catch (err) {
+      console.error(err);
+      Alert.fire({
+        icon: "error",
+        text: "Something went wrong",
+      });
+    } finally {
+      reset();
+    }
   };
   return (
     <Formik
@@ -28,6 +59,7 @@ const RegisterForm = () => {
             placeholder="Full name"
             label="Your name"
             name="fullName"
+            disabled={spinner}
             required
           />
         </div>
@@ -36,31 +68,49 @@ const RegisterForm = () => {
           placeholder="Email address"
           label="Your email"
           name="email"
+          disabled={spinner}
           required
         />
         <Password
           name="password"
           placeholder="Strong password"
           label="Password"
+          disabled={spinner}
           required
         />
         <Password
           name="confirmPassword"
           placeholder="Confirm password"
           label="Confirm password"
+          disabled={spinner}
           required
         />
         <Button
           className="w-full rounded-full"
           color="primary"
           size="sm"
+          disabled={spinner}
           type="submit"
         >
-          Register
+          {spinner ? <Spinner color="info" /> : "Register"}
         </Button>
       </Form>
     </Formik>
   );
+};
+
+const createUserData = async (userData) => {
+  await createUser(userData);
+  Alert.fire({
+    icon: "success",
+    title: "Account is created!",
+  });
+  await signIn("credentials", {
+    email: userData.email,
+    password: userData.password,
+    redirect: true,
+    callbackUrl: "/",
+  });
 };
 
 export default RegisterForm;
