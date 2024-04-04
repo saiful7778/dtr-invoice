5d04d8bc76524cce47c2fa1213f7224c47f27abf
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-04-04 20:53:22 +0600

    update style

--- src/components/formik/Input.jsx
+++ src/components/formik/Input.jsx
@@ -7,9 +7,12 @@ const InputComp = ({ placeholder, type, name, label, disabled, ...props }) => {
   const [field, { error, touched }] = useField({ name });
   return (
     <fieldset className="w-full max-w-md">
-      <Label htmlFor={inputID}>{label}</Label>
+      <Label htmlFor={inputID} className="dark:text-gray-100">
+        {label}
+      </Label>
       <Input
-        className="text-gray-100 mt-1 bg-transparent"
+        className="dark: mt-1 bg-transparent
+        text-gray-100"
         id={inputID}
         type={type}
         placeholder={placeholder}
