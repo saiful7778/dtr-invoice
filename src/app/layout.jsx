bcf82ebfc06d5abe50d83016e6f64d6b52df9766
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-04-18 21:57:25 +0600

    add  state context provider

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import SessionContextProvider from "@/context/SessionContext";
+import StateContextProvider from "@/context/StateContext";
 
 const poppins = Poppins({
   weight: ["700", "600", "500", "400"],
@@ -16,7 +17,13 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <SessionContextProvider>{children}</SessionContextProvider>
+        <SessionContextProvider>
+          <StateContextProvider>
+            <div className="relative min-h-screen w-full overflow-x-hidden bg-gray-50 text-dark-blue duration-200 dark:bg-gray-900 dark:text-gray-50">
+              {children}
+            </div>
+          </StateContextProvider>
+        </SessionContextProvider>
       </body>
     </html>
   );
