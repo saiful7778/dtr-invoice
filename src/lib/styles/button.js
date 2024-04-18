cbc874c3b6a18cd463fd4bb5d447a1360fd7e7e1
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-04-18 21:56:45 +0600

    update style

--- src/lib/styles/button.js
+++ src/lib/styles/button.js
@@ -1,7 +1,7 @@
 const style = {
   base: "rounded-md font-medium cursor-pointer shadow duration-200 active:focus:scale-95 disabled:cursor-not-allowed disabled:opacity-50",
   primary: "bg-accent-color text-white",
-  outline: "border border-accent-color hover:bg-accent-color text-white",
+  outline: "border border-accent-color hover:bg-accent-color hover:text-white",
   confirm:
     "border border-green-600 bg-green-700 text-white hover:bg-green-600 disabled:hover:bg-green-700",
   cancel: "border border-red-600 bg-red-700 text-white hover:bg-red-600",
