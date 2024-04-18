b889605db0d790e759852dbd640b51b612ff506a
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-04-18 17:38:51 +0600

    add google auth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,11 +1,16 @@
 import CredentialProvider from "next-auth/providers/credentials";
 import authConfig from "@/lib/config/auth.config";
 import { loginSchema } from "@/lib/schemas/authentication";
+import Google from "next-auth/providers/google";
 import { compare } from "bcryptjs";
 import db from "@/lib/db";
 
 const auth = {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRETS,
+    }),
     CredentialProvider({
       async authorize(credentials) {
         const isValid = await loginSchema.isValid(credentials);
@@ -13,18 +18,19 @@ const auth = {
         if (!isValid) return null;
 
         try {
-          const user = await db.user.findUnique({
+          const user = await db.user.findFirst({
             where: { email: credentials.email },
           });
 
           if (!user) return null;
 
-          const isCorrect = await compare(
-            credentials.password,
-            user.hashedPassword,
-          );
-
-          if (!isCorrect) return null;
+          if (user?.hashedPassword) {
+            const isCorrect = await compare(
+              credentials.password,
+              user.hashedPassword,
+            );
+            if (!isCorrect) return null;
+          }
 
           return {
             name: user.name,
