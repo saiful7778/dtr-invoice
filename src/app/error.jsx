7e27cf524e478920660fb6fc771a808e5116eece
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-04-19 16:39:37 +0600

    add error page

---
+++ src/app/error.jsx
@@ -0,0 +1,24 @@
+"use client";
+import Button from "@/components/Button";
+import { useEffect } from "react";
+
+const Error = ({ error, reset }) => {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+  return (
+    <main className="flex h-screen w-full flex-col items-center justify-center gap-4 overflow-hidden">
+      <h2 className="text-2xl font-bold">
+        Something went <span className="text-red-600">wrong!</span>
+      </h2>
+      <code>{error.message}</code>
+      <div>
+        <Button onClick={() => reset()} variant="cancel">
+          Refrash
+        </Button>
+      </div>
+    </main>
+  );
+};
+
+export default Error;
