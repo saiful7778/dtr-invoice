f7aa7092d739e016c407e995ce377def5fbe3498
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-04-19 16:39:45 +0600

    add loading page

---
+++ src/app/loading.jsx
@@ -0,0 +1,13 @@
+import { ImSpinner9 } from "react-icons/im";
+
+const Loading = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center overflow-hidden">
+      <span role="status">
+        <ImSpinner9 className="animate-spinner" size={60} />
+      </span>
+    </div>
+  );
+};
+
+export default Loading;
