7b8e4c5a428abfb98558433cf39f240c14d723f6
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-04-26 18:24:49 +0600

    update

--- src/lib/CURD/readData.js
+++ src/lib/CURD/readData.js
@@ -1,6 +1,7 @@
-export default async function readData(apiRoute) {
+export default async function readData(apiRoute, options) {
   try {
     const res = await fetch(process.env.NEXT_PUBLIC_API_URL + apiRoute, {
+      ...options,
       method: "GET",
       headers: {
         "content-type": "application/json",
