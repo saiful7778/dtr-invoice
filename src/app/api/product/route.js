3532f8080cac2a88d3357271270e3d634d84f074
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-05-09 03:11:08 +0600

    add product search api route

---
+++ src/app/api/product/route.js
@@ -0,0 +1,31 @@
+import db from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET(req) {
+  try {
+    const q = await req.nextUrl.searchParams.get("q");
+
+    const data = await db.product.findMany({
+      where: {
+        productName: {
+          contains: q,
+          mode: "insensitive",
+        },
+      },
+      select: {
+        id: true,
+        productName: true,
+        sell: true,
+        quantity: true,
+      },
+    });
+
+    return NextResponse.json({ success: true, data }, { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { success: false, message: "server error" },
+      { status: 500 },
+    );
+  }
+}
