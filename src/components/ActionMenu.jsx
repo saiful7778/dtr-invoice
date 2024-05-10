69d616c3af47f748e23af7cc64e774162ca61c54
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-05-10 09:35:28 +0600

    update style

--- src/components/ActionMenu.jsx
+++ src/components/ActionMenu.jsx
@@ -6,6 +6,13 @@ import { Popover } from "keep-react";
 import cn from "@/lib/utils/cn";
 import { button, focus } from "@/lib/styles";
 
+const style = {
+  button: {
+    base: "flex w-full items-center justify-between gap-4 px-2 py-1 hover:bg-gray-300 dark:hover:bg-gray-600",
+    icon: "!fill-gray-900 dark:!fill-gray-50",
+  },
+};
+
 const ActionMenu = ({ position, handleDelete, handleUpdate }) => {
   return (
     <div className="flex justify-center">
@@ -23,28 +30,16 @@ const ActionMenu = ({ position, handleDelete, handleUpdate }) => {
           <BsThreeDotsVertical size={15} />
         </Popover.Action>
         <Popover.Content className="popover z-20 overflow-hidden rounded border border-gray-300 bg-gray-50 shadow-md dark:border-gray-600 dark:bg-gray-700 dark:text-white">
-          <button
-            onClick={handleDelete}
-            className="flex w-full items-center justify-between gap-4 px-2 py-1 hover:bg-gray-300 dark:hover:bg-gray-600"
-          >
+          <button onClick={handleDelete} className={style.button.base}>
             <span>Delete</span>
             <span>
-              <FaTrashCan
-                className="!fill-gray-900 dark:!fill-gray-50"
-                size={14}
-              />
+              <FaTrashCan className={style.button.icon} size={14} />
             </span>
           </button>
-          <button
-            onClick={handleUpdate}
-            className="flex w-full items-center justify-between gap-4 px-2 py-1 hover:bg-gray-300 dark:hover:bg-gray-600"
-          >
-            <span>Edit</span>
+          <button onClick={handleUpdate} className={style.button.base}>
+            <span>Update</span>
             <span>
-              <CiEdit
-                className="!fill-gray-900 dark:!fill-gray-50"
-                strokeWidth={1}
-              />
+              <CiEdit className={style.button.icon} strokeWidth={1} />
             </span>
           </button>
         </Popover.Content>
